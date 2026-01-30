74b0f857b3d7dc0f5a2c5af32307584a5692e7c9
Amir <amirmahdibz2007@gmail.com>
2026-01-30 21:17:11 +0330

    Define BlockType enum and Block structure

---
+++ BLOCK.h
@@ -0,0 +1,26 @@
+//
+// Created by َAmir on 1/30/2026.
+//
+
+#ifndef SCRATCHCLONE_SDL_BLOCK_H
+#define SCRATCHCLONE_SDL_BLOCK_H
+
+#include <vector>
+#include <string>
+
+enum struct BlockType {
+    MOVE , TURN_RIGHT , TURN_LEFT , GO_TO_XY ,
+    SAY , SHOW , HIDE ,
+    WAIT , REPEAT , IF , FOREVER ,
+    END_REPEAT , END_IF ,
+    ON_FLAG_CLICKED
+};
+
+struct Block {
+    BlockType type;
+    std::vector<double> parameters;
+    std::string textParam;
+    int jumpToIndex = -1;
+};
+
+#endif //SCRATCHCLONE_SDL_BLOCK_H
