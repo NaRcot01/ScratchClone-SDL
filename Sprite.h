293e17131fb2e60f58cff6c01a8408f42bf41388
Amir <amirmahdibz2007@gmail.com>
2026-01-30 21:58:58 +0330

    Define Sprite structure for character state

---
+++ Sprite.h
@@ -0,0 +1,23 @@
+//
+// Created by Amir on 1/30/2026.
+//
+
+#ifndef SCRATCHCLONE_SDL_SPRITE_H
+#define SCRATCHCLONE_SDL_SPRITE_H
+
+#include <string>
+#Include <vector>
+
+struct Sprite {
+    std::string name;
+    double x =0.0 , y=0.0 ;
+    double direction = 90.0;
+    bool isVisible = true;
+    double size = 100.0;
+    int costumeIndex = 0;
+};
+
+
+
+
+#endif //SCRATCHCLONE_SDL_SPRITE_H
